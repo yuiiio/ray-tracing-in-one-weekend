7ffe3a4a4598874bdd7c43e075c9463332da9bdf
yuiiio <atbjyk@protonmail.com>
2020-02-10 23:26:13 +0900

    anti-alias

---
+++ src/camera.rs
@@ -0,0 +1,23 @@
+use crate::ray::{Ray};
+use crate::vec3::{Vector3, vec3_add, vec3_mul_b};
+
+pub struct Camera {
+    origin: Vector3<f64>,
+    lower_left_corner: Vector3<f64>,
+    horizontal: Vector3<f64>,
+    vertical: Vector3<f64>,
+}
+
+impl Camera {
+    pub fn new() -> Camera {
+        let origin = [0.0, 0.0, 0.0];
+        let lower_left_corner = [-2.0, -1.0, -1.0];
+        let horizontal = [4.0, 0.0, 0.0];
+        let vertical = [0.0, 2.0, 0.0];
+        Camera {origin, lower_left_corner, horizontal, vertical}
+    }
+
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray {a: self.origin, b: vec3_add(vec3_add(self.lower_left_corner, vec3_mul_b(self.horizontal, u)), vec3_mul_b(self.vertical, v))}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+use rand::prelude::*;
+
 mod vec3;
 mod ray;
 mod hitablelist;
 mod hitable;
 mod sphere;
+mod camera;
 
-use vec3::{Vector3, vec3_unit_vector_f64, vec3_mul_b, vec3_add, vec3_add_b};
+use vec3::{Vector3, vec3_unit_vector_f64, vec3_mul_b, vec3_add, vec3_add_b, vec3_div_b};
 use ray::{Ray};
 use hitable::{Hitable};
 use hitablelist::{HitableList};
 use sphere::{Sphere};
+use camera::{Camera};
 
 fn color(r:  &Ray, world: &HitableList) -> Vector3<f64> {
     match world.hit(r, 0.0, 1000.0) {
@@ -24,24 +28,27 @@ fn color(r:  &Ray, world: &HitableList) -> Vector3<f64> {
 fn main() {
     let nx: u32 = 200;
     let ny: u32 = 100;
+    let ns: u32 = 100; //anti-aliasing sample-per-pixel
+    let mut rng = rand::thread_rng();
     println!("P3\n {} {} \n255\n", nx, ny);
-    let lower_left_corner = [-2.0, -1.0, -1.0];
-    let horizontal = [4.0, 0.0, 0.0];
-    let vertical = [0.0, 2.0, 0.0];
-    let origin = [0.0, 0.0, 0.0];
     let mut obj_list = HitableList::new();
     obj_list.push(Sphere { center: [0.0 , 0.0 , -1.0], radius: 0.5 });
     obj_list.push(Sphere { center: [0.0, -100.5, -1.0], radius: 100.0 });
 
+    let cam = Camera::new();
+
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let u: f64 = i as f64/ nx as f64; 
-            let v: f64 = j as f64/ ny as f64; 
-            let r = Ray {
-                a: origin,
-                b: vec3_add(vec3_add(lower_left_corner, vec3_mul_b(horizontal, u)), vec3_mul_b(vertical, v))
-            };
-            let col = color(&r, &obj_list);
+            let mut col = [0.0, 0.0, 0.0];
+            for _s in 0..ns {
+                let rand_x: f64 = rng.gen();
+                let rand_y: f64 = rng.gen();
+                let u: f64 = (i as f64 + rand_x )/ nx as f64; 
+                let v: f64 = (j as f64 + rand_y )/ ny as f64; 
+                let r = cam.get_ray(u, v);
+                col = vec3_add(color(&r, &obj_list), col);
+            }
+            col = vec3_div_b(col, ns as f64);
             let ir: u32 = (255.99 * col[0]) as u32;
             let ig: u32 = (255.99 * col[1]) as u32;
             let ib: u32 = (255.99 * col[2]) as u32;
