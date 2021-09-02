76359f373e4e99d2d5082f944d64fd6811fed438
yuiiio <atbjyk@protonmail.com>
2021-09-03 01:18:57 +0900

    build_bvh: change quick_sort to merge_sort

--- src/bvh_node.rs
+++ src/bvh_node.rs
@@ -42,6 +42,79 @@ fn box_z_compare(a: &Box<dyn Hitable + Send + Sync>, b: &Box<dyn Hitable + Send 
         return false;
     }
 }
+
+fn dmerge(
+    vec: &mut Vec<usize>,
+    stock_vec: &mut Vec<usize>,
+    compare: fn(&Box<dyn Hitable + Send + Sync>, &Box<dyn Hitable + Send + Sync>) -> bool,
+    hitable_list: &HitableList,
+    left: usize,
+    mid: usize,
+    right: usize,
+) {
+    let mut i: usize = left;
+    let mut j: usize = mid;
+    let mut k: usize = 0;
+    let mut l: usize = 0;
+
+    while i < mid && j < right {
+        if compare(&hitable_list[vec[i]], &hitable_list[vec[j]]) {
+            stock_vec[k] = vec[i].clone();
+            i = i + 1;
+        } else {
+            stock_vec[k] = vec[j].clone();
+            j = j + 1;
+        }
+        k = k + 1;
+    }
+
+    if i == mid {
+        while j < right {
+            stock_vec[k] = vec[j].clone();
+            j = j + 1;
+            k = k + 1;
+        }
+    } else {
+        while i < mid {
+            stock_vec[k] = vec[i].clone();
+            i = i + 1;
+            k = k + 1;
+        }
+    }
+
+    while l < k {
+        vec[left + l] = stock_vec[l].clone();
+        l = l + 1;
+    }
+}
+
+pub fn dmerge_sort(
+    vec: &mut Vec<usize>,
+    stock_vec: &mut Vec<usize>,
+    compare: fn(&Box<dyn Hitable + Send + Sync>, &Box<dyn Hitable + Send + Sync>) -> bool,
+    hitable_list: &HitableList,
+    left: usize,
+    right: usize,
+) {
+    if (left == right) || (left + 1 == right) {
+        return;
+    }
+    let mid: usize = (left + right) / 2;
+    dmerge_sort(vec, stock_vec, compare, hitable_list, left, mid);
+    dmerge_sort(vec, stock_vec, compare, hitable_list, mid, right);
+    dmerge(vec, stock_vec, compare, hitable_list, left, mid, right);
+}
+
+pub fn dmerge_sort_wrap(
+    vec: &mut Vec<usize>,
+    compare: fn(&Box<dyn Hitable + Send + Sync>, &Box<dyn Hitable + Send + Sync>) -> bool,
+    hitable_list: &HitableList,
+) {
+    let mut stock_vec = vec.clone();
+    let len = vec.len();
+    dmerge_sort(vec, &mut stock_vec, compare, hitable_list, 0, len);
+}
+
 pub fn dqsort(
     vec: &mut Vec<usize>,
     compare: fn(&Box<dyn Hitable + Send + Sync>, &Box<dyn Hitable + Send + Sync>) -> bool,
@@ -98,9 +171,9 @@ fn build_bvh(hitable_list: &HitableList, handle: &mut Vec<usize>) -> BvhNode {
     let x: f64 = x * 3.0;
     let axis: usize = x as usize;
     match axis {
-        0 => dqsort(handle, box_x_compare, hitable_list),
-        1 => dqsort(handle, box_y_compare, hitable_list),
-        _ => dqsort(handle, box_z_compare, hitable_list),
+        0 => dmerge_sort_wrap(handle, box_x_compare, hitable_list),
+        1 => dmerge_sort_wrap(handle, box_y_compare, hitable_list),
+        _ => dmerge_sort_wrap(handle, box_z_compare, hitable_list),
     }
     let handle_size = handle.len();
     let (left_obj, right_obj): (
