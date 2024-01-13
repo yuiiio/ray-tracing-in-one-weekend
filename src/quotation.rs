cfb228eb80bf7dba58001681d49ae093ce6543da
yuiiio <atbjyk@protonmail.com>
2024-01-14 01:45:09 +0900

    Quotation: minor cleanup

--- src/quotation.rs
+++ src/quotation.rs
@@ -13,32 +13,64 @@ impl Rotation {
         let cos = radians.cos();
         let sin = radians.sin();
         let vec3 = vec3_mul_b(&axis, sin);
-        let q = Qotation(cos, vec3);
-        let oq = Qotation(cos, vec3_mul_b(&vec3, -1.0));
+        let q = Qotation{ w: cos, xyz: vec3 };
+        let oq = Qotation { w:cos, xyz: vec3_mul_b(&vec3, -1.0) };
         Rotation {q, oq}
     }
 
     pub fn rotate(&self, target: &Vector3<f64>) -> Vector3<f64> {
-        let x = qmul(&self.q, &Qotation(0.0, *target));
-        let y = qmul(&x, &self.oq);
-        y.1
+        let x = qmul_q2_xyz(&self.q, target);
+        qmul_ret_xyz(&x, &self.oq)
     }
 }
 
 #[derive(Clone)]
-pub struct Qotation(pub f64, pub [f64; 3]); //[w] [x, y, z]
+pub struct Qotation {
+    pub w: f64,
+    pub xyz: [f64; 3],
+}
 
+#[allow(dead_code)]
 pub fn qmul(q1: &Qotation, q2: &Qotation) -> Qotation {
-    let w1 = q1.0;
-    let w2 = q2.0;
-    let v1 = q1.1;
-    let v2 = q2.1;
+    let w1 = q1.w;
+    let w2 = q2.w;
+    let v1 = q1.xyz;
+    let v2 = q2.xyz;
     let inner = (v1[0] * v2[0])
                 + (v1[1] * v2[1])
                 + (v1[2] * v2[2]);
     let cross: Vector3<f64> = [ (v1[1] * v2[2]) - (v1[2] * v2[1]),
                                 (v1[2] * v2[0]) - (v1[0] * v2[2]),
                                 (v1[0] * v2[1]) - (v1[1] * v2[0]), ];
-    Qotation((w1 * w2) - inner,
-                vec3_add(&vec3_add(&vec3_mul_b(&v2, w1), &vec3_mul_b(&v1, w2)), &cross))
+    Qotation {
+        w: (w1 * w2) - inner,
+        xyz: vec3_add(&vec3_add(&vec3_mul_b(&v2, w1), &vec3_mul_b(&v1, w2)), &cross),
+    }
+}
+
+pub fn qmul_q2_xyz(q1: &Qotation, v2: &[f64; 3]) -> Qotation {
+    let w1 = q1.w;
+    let v1 = q1.xyz;
+    let inner = (v1[0] * v2[0])
+                + (v1[1] * v2[1])
+                + (v1[2] * v2[2]);
+    let cross: Vector3<f64> = [ (v1[1] * v2[2]) - (v1[2] * v2[1]),
+                                (v1[2] * v2[0]) - (v1[0] * v2[2]),
+                                (v1[0] * v2[1]) - (v1[1] * v2[0]), ];
+    Qotation {
+        w: - inner,
+        xyz: vec3_add(&vec3_mul_b(&v2, w1), &cross),
+    }
+}
+
+pub fn qmul_ret_xyz(q1: &Qotation, q2: &Qotation) -> [f64; 3] {
+    let w1 = q1.w;
+    let w2 = q2.w;
+    let v1 = q1.xyz;
+    let v2 = q2.xyz;
+    let cross: Vector3<f64> = [ (v1[1] * v2[2]) - (v1[2] * v2[1]),
+                                (v1[2] * v2[0]) - (v1[0] * v2[2]),
+                                (v1[0] * v2[1]) - (v1[1] * v2[0]), ];
+
+    vec3_add(&vec3_add(&vec3_mul_b(&v2, w1), &vec3_mul_b(&v1, w2)), &cross)
 }
