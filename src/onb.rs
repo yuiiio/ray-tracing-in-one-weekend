f82731f6729bd1e19ed1dc660328f452a6dac39d
yuiiio <atbjyk@protonmail.com>
2024-01-25 20:13:43 +0900

    onb: build_from_w fixup since 5da217bb4ba81ec028d9fd2f44951e222766eb45

--- src/onb.rs
+++ src/onb.rs
@@ -11,9 +11,9 @@ impl Onb {
     // build_from_w should take normalized vec
     pub fn build_from_w(w: &Vector3<f64>) -> Self {
         let (v, u) = if w[0].abs() > 0.9 {
-            //let b = 1.0 / (w[2].powi(2) + w[0].powi(2));
+            //let b = 1.0 / (w[2].powi(2) + w[0].powi(2)).sqrt();
             // w is normalized vec so, (w[2]^2 + w[0]^2) = (1 - w[1]^2)
-            let b = 1.0 / (1.0 - w[1].powi(2));
+            let b = 1.0 / (1.0 - w[1].powi(2)).sqrt();
             let v0 = -w[2] * b;
             let v2 = w[0] * b;
             ([
@@ -27,7 +27,7 @@ impl Onb {
             - w[1] * v0,
             ])
         } else {
-            let b = 1.0 / (1.0 - w[0].powi(2));
+            let b = 1.0 / (1.0 - w[0].powi(2)).sqrt();
             let v1 = w[2] * b;
             let v2 = -w[1] * b;
             ([
